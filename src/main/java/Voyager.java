a30ff2e5a2f5e82cc2cbb595874cbd2961511c2b
reynardian <reynardian10@gmail.com>
2026-01-22 22:50:29 +0800

    Added Echo

--- src/main/java/Voyager.java
+++ src/main/java/Voyager.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Voyager {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "_".repeat(60);
+
         System.out.println(line);
-        System.out.println("Hello from Voyager!\nWhat can I do for you?");
+        System.out.println("Hello! I'm Voyager");
+        System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break; // Exit the loop
+            }
+
+            System.out.println(input); // Echo the command
+        }
+
+        scanner.close();
     }
 }
