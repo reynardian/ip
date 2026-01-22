6b681ed4d09791011196ae6ea2cb2ca5f44156cf
reynardian <reynardian10@gmail.com>
2026-01-22 14:28:03 +0800

    Initial skeleton

---
+++ src/main/java/Voyager.java
@@ -0,0 +1,9 @@
+public class Voyager {
+    public static void main(String[] args) {
+        String line = "_".repeat(60);
+        System.out.println(line);
+        System.out.println("Hello from Voyager!\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+}
