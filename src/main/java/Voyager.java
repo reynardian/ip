35c6a9c0048545dba1d5430e2cdd5d816af2672c
reynardian <reynardian10@gmail.com>
2026-01-22 22:57:46 +0800

    Added List

--- src/main/java/Voyager.java
+++ src/main/java/Voyager.java
@@ -5,6 +5,9 @@ public class Voyager {
         Scanner scanner = new Scanner(System.in);
         String line = "_".repeat(60);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(line);
         System.out.println("Hello! I'm Voyager");
         System.out.println("What can I do for you?");
@@ -17,10 +20,24 @@ public class Voyager {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
-                break; // Exit the loop
+                break; // exit the loop
+            } else if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks added yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("added: " + input);
+                } else {
+                    System.out.println("Task list is full!");
+                }
             }
-
-            System.out.println(input); // Echo the command
         }
 
         scanner.close();
