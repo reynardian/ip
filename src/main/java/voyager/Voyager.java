926ecb01834588834f8e51e821c2f45b90c6ecaa
reynardian <reynardian10@gmail.com>
2026-02-02 20:54:56 +0800

    Add find function

--- src/main/java/voyager/Voyager.java
+++ src/main/java/voyager/Voyager.java
@@ -112,7 +112,7 @@ public class Voyager {
 
                         ui.showFoundTasks(matchingTasks);
                         break;
-                        
+
                     default:
                         throw new VoyagerException(
                                 "OOPS!!! I'm sorry, but I don't know what that means :-(");
